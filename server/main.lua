ed98bce4b962e24087da332b0b065e9254fc489c
TrueSunnyD <82911075+TrueSunnyD@users.noreply.github.com>
2022-08-18 22:00:06 -0600

    fixed job error in billing

--- server/main.lua
+++ server/main.lua
@@ -6,7 +6,7 @@ AddEventHandler("qb-luchettijob:bill:player", function(playerId, amount)
         local biller = QBCore.Functions.GetPlayer(source)
         local billed = QBCore.Functions.GetPlayer(tonumber(playerId))
         local amount = tonumber(amount)
-        if biller.PlayerData.job.name == 'weedshop' then
+        if biller.PlayerData.job.name == 'luchetti' then
             if billed ~= nil then
                 if biller.PlayerData.citizenid ~= billed.PlayerData.citizenid then
                     if amount and amount > 0 then
